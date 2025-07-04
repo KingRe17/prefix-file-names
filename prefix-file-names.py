d82e223f9b399af0491d0de03e994c6bdbb551db
KingRe17 <reynaldo17r@yahoo.com.br>
2025-07-04 11:34:15 -0300

    Add files via upload

---
+++ prefix-file-names.py
@@ -0,0 +1,21 @@
+﻿import glob
+import os
+
+while True:
+ local = input('local = ')
+ 
+ tudo = glob.glob(local + '\\*')
+ print(tudo)
+ 
+ nova = input('word = ')         # the word you want to add
+ 
+ for i in range(len(tudo)):
+ 
+  arqu = os.path.basename(tudo[i])  # receive 1 file from the list (only name without path)             
+  f = nova + arqu
+ 
+  fim = local + '\\' + f
+ 
+  os.rename(tudo[i], fim)
+
+
